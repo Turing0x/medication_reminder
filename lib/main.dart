1d9978117fa76e06f8d06c14b403b323762075f7
Turing0x <rauligf97@gmail.com>
2025-04-13 20:48:24 -0400

    feat(app): update dependencies and configure launcher icons
    - Add new dependencies for `archive`, `flutter_launcher_icons`, `image`, and `posix` packages
    - Configure `flutter_launcher_icons` for custom launcher icon and set image path
    - Update AndroidManifest.xml to reflect new app label and launcher icon
    - Enhance main.dart with custom theme and font settings
    
    This update improves the app's appearance and functionality by integrating new packages and customizing the launcher icon.

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,74 @@ class MyApp extends StatelessWidget {
       title: 'Medication Reminder',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
+        fontFamily: 'Athiti',
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF5A4AA4),
+          onPrimary: Colors.white,
+          secondary: Color(0xFF7B50C5),
+          onSecondary: Colors.white,
+          tertiary: Color(0xFF71599B),
+          surface: Colors.white,
+          error: Color(0xFFDC3545),
+        ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 244, 244, 245),
+        navigationBarTheme: NavigationBarThemeData(
+          backgroundColor: Color.fromARGB(255, 244, 244, 245),
+          indicatorColor: Colors.white,
+          labelTextStyle:
+              WidgetStateProperty.all(TextStyle(color: Color(0xFF5A4AA4))),
+          iconTheme:
+              WidgetStateProperty.all(IconThemeData(color: Color(0xFF5A4AA4))),
+        ),
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF333227),
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: TextStyle(
+            color: Color(0xFF333227),
+            fontWeight: FontWeight.w600,
+          ),
+          bodyLarge: TextStyle(
+            color: Color(0xFF333227),
+          ),
+        ),
+        cardTheme: CardTheme(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          color: Colors.white,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF5A4AA4),
+          foregroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: TextStyle(fontWeight: FontWeight.w500),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Color(0xFF5E5370)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Color(0xFF5E5370)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Color(0xFF5A4AA4)),
+          ),
+        ),
       ),
       home: const MedicationListScreen(),
     );
